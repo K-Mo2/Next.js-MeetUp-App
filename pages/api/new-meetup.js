85364c77a23695fc05e7d931508d541f7105bf2d
K-Mo2 <karimelsisi27@gmail.com>
2021-11-08 01:28:38 +0200

    working with mongodb

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://new-user31:4KvEdddMdN0dE2dG@cluster0.rltnc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted successfully" });
+  }
+}
+
+export default handler;
