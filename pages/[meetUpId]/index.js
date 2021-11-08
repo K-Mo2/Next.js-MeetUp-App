770065be9aef3c3d7db74ced2f557e72d2213f8b
K-Mo2 <karimelsisi27@gmail.com>
2021-11-08 10:56:44 +0200

    fixing a bug in the static paths

--- pages/[meetUpId]/index.js
+++ pages/[meetUpId]/index.js
@@ -35,7 +35,7 @@ export async function getStaticPath() {
   client.close();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
